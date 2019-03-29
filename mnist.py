96d82c17bf32bb1dba8b4afb47ba397c78d86051
Scott Bishop <programmingbishop@gmail.com>
2019-03-29 17:11:32 -0500

    More Image Augmentation Steps

--- mnist.py
+++ mnist.py
@@ -13,18 +13,22 @@ import matplotlib.pyplot as plt
 import seaborn           as sns
 
 # Preprocessing
+from sklearn.model_selection import train_test_split
+
 from keras.preprocessing.image import ImageDataGenerator
 from keras.utils.np_utils      import to_categorical # One-Hot-Encode Labels
+from keras.models              import Sequential
+from keras.layers              import Convolution2D, MaxPooling2D, Flatten, Dense
 
 
 # Global Functions
 # ==================================================
 def show_images( start_obs, df ):
     '''
-    start_obs: index to start for digit display
-    df: pandas dataframe start_obs indexes from
+    start_obs: Index to start for digit display
+    df: Pandas dataframe start_obs indexes from
     '''
-    for index, image in enumerate( range( start_obs, ( start_obs + 9 ) ) ):
+    for index, _ in enumerate( range( start_obs, ( start_obs + 9 ) ) ):
         plt.subplot( 330 + 1 + index )
         plt.imshow( np.matrix( df[ index ] ), 
                     cmap = plt.get_cmap( 'gray' ) ) 
@@ -32,6 +36,10 @@ def show_images( start_obs, df ):
 
 
 def return_flow_batch( X, y ):
+    '''
+    X: Feature values
+    y: Response values
+    '''
     for X_batch, _ in datagen.flow( X, y, batch_size = 9 ):
         for image in range( 0, 9 ):
             plt.subplot( 330 + 1 + image )
@@ -39,6 +47,10 @@ def return_flow_batch( X, y ):
                         cmap = plt.get_cmap( 'gray' ) )
         plt.show()
         break
+
+
+def return_nan_count( df ):
+    print( df.isnull().any().describe() )
 
 
 # Load Data
@@ -50,6 +62,11 @@ data_path = os.getcwd() + '\\..\\..\\data\\mnist\\'
 full_train = pd.read_csv( data_path + 'train.csv' )
 full_test  = pd.read_csv( data_path + 'test.csv' )
 
+# Check for Missing
+return_nan_count( full_train )
+return_nan_count( full_test )
+
+# Reshape 2D Matrix into 4D
 train_X = full_train\
             .drop( [ 'label' ], axis = 1 )\
             .astype( 'float32' )\
@@ -69,6 +86,18 @@ test_X = full_test\
 show_images( 0, train_X )
 
 
+# Train / Validation Split
+# ==================================================
+VALIDATION_SIZE = 0.2
+
+X_train, X_valid, y_train, y_valid = train_test_split( 
+    train_X, 
+    train_y, 
+    test_size    = VALIDATION_SIZE, 
+    random_state = 19920917 
+)
+
+
 # Image Augmentation
 # ==================================================
 datagen = ImageDataGenerator(
@@ -77,8 +106,10 @@ datagen = ImageDataGenerator(
     zca_whitening                 = True
 )
 
-datagen.fit( train_X )
+datagen.fit( X_train )
 
+# Preview Image Augmentations
+return_flow_batch( X_train, y_train )
 
 
 # Generate Submission
