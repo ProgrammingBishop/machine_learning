f03bba6c0d33365737cb758a6efb4812a734ca33
Scott Bishop <programmingbishop@gmail.com>
2019-03-29 18:05:57 -0500

    Begin building CNN Architecture

--- mnist.py
+++ mnist.py
@@ -13,12 +13,14 @@ import matplotlib.pyplot as plt
 import seaborn           as sns
 
 # Preprocessing
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, RandomizedSearchCV
 
-from keras.preprocessing.image import ImageDataGenerator
-from keras.utils.np_utils      import to_categorical # One-Hot-Encode Labels
-from keras.models              import Sequential
-from keras.layers              import Convolution2D, MaxPooling2D, Flatten, Dense
+from keras.wrappers.scikit_learn import KerasClassifier
+from keras.preprocessing.image   import ImageDataGenerator
+from keras.utils.np_utils        import to_categorical # One-Hot-Encode Labels
+from keras.models                import Sequential
+from keras.optimizers            import RMSprop, Adam
+from keras.layers                import Conv2D, MaxPool2D, Flatten, Dense, Dropout
 
 
 # Global Functions
@@ -66,6 +68,10 @@ full_test  = pd.read_csv( data_path + 'test.csv' )
 return_nan_count( full_train )
 return_nan_count( full_test )
 
+# Normalize Values
+full_train = full_train / 255.0
+full_test  = full_test  / 255.0
+
 # Reshape 2D Matrix into 4D
 train_X = full_train\
             .drop( [ 'label' ], axis = 1 )\
@@ -101,15 +107,168 @@ X_train, X_valid, y_train, y_valid = train_test_split(
 # Image Augmentation
 # ==================================================
 datagen = ImageDataGenerator(
-    featurewise_center            = True, 
-    featurewise_std_normalization = True,
-    zca_whitening                 = True
-)
+    featurewise_center            = False, 
+    featurewise_std_normalization = False,
+    samplewise_std_normalization  = False,  
+    zca_whitening                 = True,
+    rotation_range                = 90,
+    zoom_range                    = 0.25,
+    width_shift_range             = 0.1,
+    height_shift_range            = 0.1,
+    horizontal_flip               = False,
+    vertical_flip                 = False
+)  
+
 
 datagen.fit( X_train )
 
 # Preview Image Augmentations
 return_flow_batch( X_train, y_train )
+
+
+# Construct Convolutional Neural Network
+# ==================================================
+# Build CNN Architecure
+def build_cnn():
+    '''
+    optimizer: 1 = RMSProp / 2 = Adam
+    '''
+    model = Sequential( optimizer = 'RMSProp' )
+
+    model.add( 
+        Conv2D( filters     = 32, 
+                kernel_size = ( 5, 5 ), 
+                strides     = ( 1, 1 ),
+                padding     = 'same',
+                input_shape = ( 28, 28, 1 ), 
+                activation  = 'relu', 
+                data_format = 'channels_last' 
+        ) 
+    )
+
+    model.add( 
+        Conv2D( filters     = 32, 
+                kernel_size = ( 5, 5 ), 
+                strides     = ( 1, 1 ),
+                padding     = 'same',
+                activation  = 'relu', 
+                data_format = 'channels_last' 
+        ) 
+    )
+
+    model.add( 
+        MaxPool2D( pool_size = ( 2, 2 ) ) 
+    )
+
+    model.add(
+        Dropout( 0.25 )
+    )
+
+    model.add( 
+        Conv2D( 
+            filters     = 64, 
+            kernel_size = ( 3, 3 ), 
+            strides     = ( 1, 1 ),
+            padding     = 'same',
+            activation  = 'relu', 
+            data_format = 'channels_last' 
+        ) 
+    )
+
+    model.add( 
+        Conv2D( 
+            filters     = 64, 
+            kernel_size = ( 3, 3 ), 
+            strides     = ( 1, 1 ),
+            padding     = 'same',
+            activation  = 'relu', 
+            data_format = 'channels_last' 
+        ) 
+    )
+    model.add( 
+        MaxPool2D( 
+            pool_size = ( 2, 2 ),
+            strides   = ( 2, 2 )
+        ) 
+    )
+
+    model.add(
+        Dropout( 0.25 )
+    )
+
+    model.add( 
+        Flatten() 
+    )
+
+    model.add( 
+        Dense( 
+            units      = 256, 
+            activation = 'relu' 
+        ) 
+    )
+
+    model.add(
+        Dropout( 0.50 )
+    )
+
+    model.add( 
+        Dense( 
+            units      = 1, 
+            activation = 'softmax' 
+        ) 
+    )
+
+    # Dynamic Optimizer
+    if optimizer == 'RMSProp':
+        optimizer = RMSprop(    
+            lr      = 0.001, 
+            rho     = 0.9, 
+            epsilon = 1e-08, 
+            decay   = 0.0
+        )
+    
+    elif optimizer == 'Adam':
+        optimizer = Adam(    
+            lr     = 0.001,
+            beta_1 = 0.2,
+            beta_2 = 0.7
+        )
+
+    model.compile(
+        optimizer = optimizer, 
+        loss      = "categorical_crossentropy", 
+        metrics   = [ "accuracy" ]
+    )
+
+    return model
+
+Adam()
+
+# Run CNN Model Construction
+cnn_grid = {
+
+}
+
+mnist_classifier = KerasClassifier( build_fn = build_cnn, verbose = 0 )
+
+cnn_model_grid = RandomizedSearchCV(
+    estimator           = mnist_classifier,
+    param_distributions = cnn_grid,
+    n_jobs              = 6,
+    cv                  = 5
+)
+
+
+
+
+
+
+
+
+history = model.fit_generator(datagen.flow(X_train,Y_train, batch_size=batch_size),
+                              epochs = epochs, validation_data = (X_val,Y_val),
+                              verbose = 2, steps_per_epoch=X_train.shape[0] // batch_size
+                              , callbacks=[learning_rate_reduction])
 
 
 # Generate Submission
