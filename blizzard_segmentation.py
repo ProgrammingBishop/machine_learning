10675d8749b3b8c515cd79c953634ed84c22f0b2
ScottAndrewBishop <programmingbishop@gmail.com>
2019-04-02 21:22:42 -0500

    Loaded & Imported Libraries; Found Sources to Stream from

---
+++ blizzard_segmentation.py
@@ -0,0 +1,24 @@
+# Imports
+# ==================================================
+# Default Libraries
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import seaborn as sns 
+
+# Streaming Libraries
+import tweepy
+import facebook
+
+
+# Obtaining Twitter Data
+# ==================================================
+
+
+# Obtaining Facebook Data
+# ==================================================
+
+
+# Obtaining YouTube Data 
+# (Videos Targeted by Facebook & Twitter Trends)
+# ==================================================
