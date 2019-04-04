a7f97a4bbf520568ac839b8be7fc7131a820a703
ScottAndrewBishop <programmingbishop@gmail.com>
2019-04-03 23:13:49 -0500

    Major Refactoring

--- blizzard_sentiment.py
+++ blizzard_sentiment.py
@@ -1,127 +1,137 @@
 # Imports
 # ==================================================
 # Default Libraries
-import sys
-
 import numpy             as np 
 import pandas            as pd 
 import matplotlib.pyplot as plt 
 import seaborn           as sns 
 
+from collections import defaultdict
+
+# System Libraries
+import sys
+import time
+import os
+
 # Streaming Libraries
 from tweepy.streaming import StreamListener
-from tweepy           import OAuthHandler, Stream, Cursor, API
-
-import facebook
+from tweepy           import OAuthHandler, Stream, Cursor, API, TweepError
 
 
-# Global Variables, Classes, & Functions
+# Global Variables
 # ==================================================
-# Variables
-TWITTER_WRITE_COUNT = 0
-MAX_TWITTER_RECORDS = 1000
+# Find UserID: http://mytwitterid.com/
+TARGET_SCREEN_NAME       = 'Blizzard_Ent'
+TARGET_TWITTER_ID        = '331908924'
+TARGET_TRACK_LIST        = [ 
+    'Overwatch',   'Warcraft', 
+    'Hearthstone', 'Diablo', 
+    'Blizzard',    'StarCraft' 
+]
+TARGET_STATUS_COUNT      = 0
+TARGET_MAX_STATUSES      = 1000
+MAX_FOLLOWERS            = 2
+
+# Securly Obtain Credentials
+credentials = os.fspath( os.getcwd() )
+move_up_dir = os.path.relpath( 'C:\\Users\\andre\\Desktop\\twitter_credentials', credentials )
+credentials = os.path.join( credentials, move_up_dir + '\\twitter_credentials.csv' )
+credentials = pd.read_csv( credentials, delimiter = ',', index_col = None )
+
+# Credentials
+ACCESS_TOKEN        = credentials[ 'access_token'        ][0]
+ACCESS_TOKEN_SECRET = credentials[ 'access_token_secret' ][0]
+CONSUMER_KEY        = credentials[ 'consumer_key'        ][0]
+CONSUMER_SECRET     = credentials[ 'consumer_secret'     ][0]
+
+# Save File Locations
+TARGET_STREAM_DATA   = '.\\blizzard_stream_data.txt'
+TARGET_STATUSES_DATA = '.\\blizzard_statuses_data.txt'
+FOLLOWER_FRIEND_DATA = '.\\blizzard_follower_friends.txt'
+
+
+# Functions
+# ==================================================
+def write_to_file( filepath, content_to_write ):
+    with open( filepath, 'a', encoding = 'utf-8' ) as f:
+            f.write( content_to_write ).close()
+
 
 # Classes
+# ==================================================
 class Listener( StreamListener ):
     def on_data( self, data ):
-        global TWITTER_WRITE_COUNT
-        global MAX_TWITTER_RECORDS
+        '''
+        Saves Twitter stream data into text file
+        --------------------------------------------------
+        data     : Twitter information retreived by Stream() object
+        filename : Location to write data to
+        '''
+        global TARGET_STATUS_COUNT
+        global TARGET_MAX_STATUSES
 
-        # Write to File
-        with open( '.\\blizzard_sentiment.txt', 'a', encoding = 'utf-8' ) as f:
-            f.write( data )
-            f.close()
+        write_to_file( TARGET_STREAM_DATA, data )
 
-        # Record Progress in Console
-        TWITTER_WRITE_COUNT += 1
-
-        print( 
-            data + '\n' +
-            str( TWITTER_WRITE_COUNT )
-        )
-
-        # End after N Status Records
-        if TWITTER_WRITE_COUNT >= MAX_TWITTER_RECORDS:
+        if TARGET_STATUS_COUNT >= TARGET_MAX_STATUSES:
             sys.exit()
+        else:
+            TARGET_STATUS_COUNT += 1
+            print( "Progress: {}%"\
+                .format( str( round( TARGET_STATUS_COUNT / TARGET_MAX_STATUSES * 100, 2 ) ) ) )
         
     def on_error( self, status ):
         print( status )
 
-# Functions
+
+class GetFromTwitter():
+    def get_follower_friends( self, user_name, api ):
+        '''
+        Saves dictionary of user's follower's friends.
+        --------------------------------------------------
+        user_name : Twitter profile getting followers from
+        api       : teepy API() object
+        '''
+        global MAX_FOLLOWERS
+
+        follower_friends = defaultdict( list )
+
+        for follower in Cursor( api.followers, screen_name = user_name, ).items( MAX_FOLLOWERS ):
+            follower_friends[ '"' + follower.screen_name +  '"' ]\
+                .append( api.friends_ids( screen_name = follower.screen_name ) )
+
+        write_to_file( FOLLOWER_FRIEND_DATA, follower_friends )
 
 
-# Obtaining Twitter Data
-# Find UserID: http://mytwitterid.com/
+# Obtaining Data
 # ==================================================
-# Securly Obtain Credentials
-credentials = 'C: Users andre Desktop GitHub Data twitter_credentials twitter_credentials.csv'\
-    .replace( " ", "\\" )
-
-credentials = pd.read_csv( 
-    credentials,
-    delimiter = ',',
-    index_col = None 
-)
-
-# Credentials
-access_token        = credentials[ 'access_token'        ][0]
-access_token_secret = credentials[ 'access_token_secret' ][0]
-consumer_key        = credentials[ 'consumer_key'        ][0]
-consumer_secret     = credentials[ 'consumer_secret'     ][0]
-
 # Run Stream
-if __name__ == '__main__':
+if __name__ == '__main__':   
     # Open Connection
-    listener  = Listener()
-    authorize = OAuthHandler( 
-        consumer_key, 
-        consumer_secret 
+    get_from_twitter = GetFromTwitter()
+    listener         = Listener()
+    authorize        = OAuthHandler( CONSUMER_KEY, CONSUMER_SECRET )
+
+    authorize.set_access_token( ACCESS_TOKEN, ACCESS_TOKEN_SECRET )
+
+    # Build Stream
+    api    = API( authorize, wait_on_rate_limit = True )
+    stream = Stream( auth = authorize, listener = listener )
+
+    # Retrieve/Write Statuses
+    target_statuses = api.user_timeline( 
+        screen_name = TARGET_SCREEN_NAME, 
+        count       = TARGET_MAX_STATUSES, 
+        include_rts = True 
     )
 
-    authorize.set_access_token( 
-        access_token, 
-        access_token_secret 
+    write_to_file( TARGET_STATUSES_DATA, target_statuses )
+
+    # Retrieve/Write Follower Friends
+    get_from_twitter.get_follower_friends( TARGET_SCREEN_NAME, api )
+
+    # Retrieve/Write Streamed Statuses
+    stream.filter( 
+        follow   = TARGET_TWITTER_ID,
+        track    = TARGET_TRACK_LIST, 
+        encoding = 'utf8'
     )
-
-    api             = API( authorize )
-
-    # Build Stream Target
-    user_stream     = Stream( auth = authorize, listener = listener )
-    blizzard_stream = Stream( auth = authorize, listener = listener )
-
-    # Initialize Stream
-    try:
-        # Obtain N Recent Statuses
-        # blizzard = api.user_timeline( 
-        #     screen_name = 'Blizzard_Ent', 
-        #     count       = 100, 
-        #     include_rts = True 
-        # )
-
-        # Stream Incoming Statuses
-        user_stream.filter( 
-            # Blizzard Entertainment
-            follow   = '331908924',
-            track    = [ 
-                'Overwatch',   'Warcraft', 
-                'Hearthstone', 'Diablo', 
-                'Blizzard',    'StarCraft' 
-            ], 
-            encoding = 'utf8'
-        )
-
-    except KeyboardInterrupt:
-        print( "Done" )
-
-    finally:
-        user_stream.disconnect()
-
-
-# Obtaining Facebook Data
-# ==================================================
-
-
-
-# Obtaining YouTube Data 
-# (Videos Targeted by Facebook & Twitter Trends)
-# ==================================================
