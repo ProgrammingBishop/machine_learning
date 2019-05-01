bffa35d400ec9aa3eb3dc8b2ba7cc7f003741abf
ScottAndrewBishop <programmingbishop@gmail.com>
2019-05-01 18:48:29 -0500

    begin NLP

--- tweepy/explore_twitter_data/GetDescriptors.py
+++ tweepy/explore_twitter_data/GetDescriptors.py
@@ -3,8 +3,13 @@ import ast
 import pandas         as pd
 import configurations as c
 
+from scipy import spatial
+from Utilities       import Utilities
+
 class GetDescriptors():
     # PRIVATE
+    utilities   = Utilities()
+    punctuation = '\n\n \n\n\n!"-#$%&()--.*+,-/:;<=>?@[\\]^_`{|}~\t\n '
     __nlp = ''
     __str = ''
     
@@ -13,53 +18,64 @@ class GetDescriptors():
 
 
     # PUBLIC
-    def clean_text( self, doc_text ):
-        return [ token.text.lower() for token in self.__nlp( doc_text ) if token.text not in '\n\n \n\n\n!"-#$%&()--.*+,-/:;<=>?@[\\]^_`{|}~\t\n ' and token.text not in self.__nlp.Defaults.stop_words ]
+    def clean_text( self, description ):
+        return_token = []
+        token_lower  = ()
+
+        for token in self.__nlp( description ):
+            token_lower = token.text.lower()
+
+            if token_lower not in self.punctuation:
+                if token_lower not in self.__nlp.Defaults.stop_words:
+                    if token_lower in self.__nlp.vocab:
+                        if len( token_lower ) > 2:
+                            return_token.append( token_lower )
+
+        print( return_token )
+        return return_token
 
 
     def get_descriptors( self ):
-        # Create List of all IDs
+        # Create List of all IDs6
         data        = pd.read_csv( c.STREAM_DATAFRAME_CSV )
         # descriptors = data[ 'description' ].apply( lambda desc : ast.literal_eval( desc ) )
         unique_desc = []
 
-        for desc in data[ 'description' ]:
-            self.__str = self.__nlp( u'"' + str( desc ) + '"' )
-            print( self.clean_text( str( desc ) ) )
-            print( '\n' )
-            
-            # for token in self.__str:
-            #     print( token.text )
-            #     print( '\n' )
-            # self.__str   = self.__nlp( u'"' + desc + '"' )
-            # unique_desc.append( self.__str )
-            # unique_desc += self.__str
+        for index, desc in enumerate( data[ 'description' ] ):
+            if index < 5:
+                self.__str = self.__nlp( u'"' + str( desc ) + '"' )
+                unique_desc += self.clean_text( str( desc ) )
 
         unique_desc = list( set( unique_desc ) )
-        print( unique_desc )
+        
+        cosine_similarity       = lambda vec1, vec2: 1 - spatial.distance.cosine( vec1, vec2 )
+        descriptor_similarities = {
+            'word'        : [],
+            'top_similar' : []
+        }
 
-        # Remove all stop words in this new list
-        # Remove Punctuation
+        print( 'Next...\n' )
 
-        # computed_similarities   = []
-        # descriptor_similarities = {
-            # word        = [],
-            # top_similar = []
-        # }
+        for index, desc in enumerate( unique_desc ):
+            if index < 5:
+                computed_similarities = []
+                a                     = self.__nlp.vocab[ str( desc ) ].vector
 
-        # For desc in descriptions
-            # desc = nlp.vocab[ desc ].vector
+                for word in self.__nlp.vocab:
+                    if word.has_vector:
+                        if word.is_lower:
+                            if word.is_alpha:
+                                similarity = cosine_similarity( a, word.vector )
+                                computed_similarities.append( ( word, similarity ) )
 
-            # for word in nlp.vocab:
-            #     if word.has_vector:
-            #         if word.is_lower:
-            #             if word.is_alpha:
-            #                 similarity = cosine_similarity( desc, word.vector )
-            #                 computed_similarities.append( ( word, similarity ) )
+                computed_similarities = sorted( computed_similarities, key = lambda item : -item[ 1 ] )
+                computed_similarities = [ t[ 0 ].text for t in computed_similarities[ :10 ] ]
 
-            # computed_similarities = sorted( computed_similarities, key = lambda item : -item[ 1 ] )
-            # computed_similarities = [ t[ 0 ].text for t in computed_similarities[ :10 ] ]
+                descriptor_similarities[ 'word'        ].append( desc )
+                descriptor_similarities[ 'top_similar' ].append( computed_similarities )
 
-            # descriptor_similarities[ 'word'        ].append( desc )
-            # descriptor_similarities[ 'top_similar' ].append( desc )
-        return
+                print( desc )
+                print( computed_similarities )
+                print( '\n' )
+
+        self.utilities.write_to_file( c.TOKENIZED_DESCRIPTIONS, pd.DataFrame( descriptor_similarities ) )
